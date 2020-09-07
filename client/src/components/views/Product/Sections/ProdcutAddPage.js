0218b049f3975a3cc57ad0cebf500942cc2796ec
ehbong <hangillw86@gmail.com>
2020-09-08 00:13:44 +0900

    add image upload form

--- client/src/components/views/Product/Sections/ProdcutAddPage.js
+++ client/src/components/views/Product/Sections/ProdcutAddPage.js
@@ -1,18 +1,31 @@
 /** @format */
 
 import React, { useState, useEffect } from "react";
-import { Typography, Button, Form, message, Input, Icon, Row, Col, Select, InputNumber } from "antd";
-import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import { Typography, Button, Form, message, Input, Icon, Row, Col, Select, InputNumber, Upload, Modal } from "antd";
+import { UserOutlined, LockOutlined, PlusOutlined } from "@ant-design/icons";
 
 const { Title } = Typography;
 const { TextArea } = Input;
 const { Option } = Select;
+
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+}
 
 function ProdcutAddPage() {
   const [ProductTitle, setProductTitle] = useState("");
   const [Description, setDescription] = useState("");
   const [Price, setPrice] = useState(0);
   const [Quantity, setQuantity] = useState(0);
+  const [FileList, setFileList] = useState([]);
+  const [PreviewImage, setPreviewImage] = useState("");
+  const [PreviewVisible, setPreviewVisible] = useState(false);
+  const [PreviewTitle, setPreviewTitle] = useState("");
 
   const changePriceHandler = (value) => {
     console.log(value);
@@ -23,6 +36,29 @@ function ProdcutAddPage() {
     console.log(value);
     setQuantity(value);
   };
+
+  const handlePreview = async (file) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+    setPreviewImage(file.url || file.preview);
+    setPreviewVisible(true);
+    setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf("/") + 1));
+  };
+
+  const handleChange = ({ fileList }) => {
+    console.log(fileList);
+    return setFileList(fileList);
+  };
+
+  const handleCancel = () => setPreviewVisible(false);
+
+  const uploadButton = (
+    <div>
+      <PlusOutlined />
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  );
 
   return (
     <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
@@ -61,7 +97,7 @@ function ProdcutAddPage() {
             />
           </Col>
           <Col span={6}>
-            <InputNumber id="productPrice" min={0} onChange={changeQuantityHandler} value={Quantity} />
+            <InputNumber id="productQuantity" min={0} onChange={changeQuantityHandler} value={Quantity} />
           </Col>
         </Row>
         <br />
@@ -90,24 +126,12 @@ function ProdcutAddPage() {
         <br />
         <br />
         <div style={{ display: "flex", justifyContent: "space-between" }}>
-          {/* <Dropzone 
-                    onDrop={onDrop}
-                    multiple={false}
-                    maxSize={10000000000}>
-                        {({ getRootProps, getInputProps })=>(
-                            <div style={{ width: '300px', height: '240px', border:'1px solid lightgray', display:'flex', alignItems: 'center', justifyContent: 'center' }}{...getRootProps()}>
-                                
-                            <input {...getInputProps()}/>
-                            <Icon type="plus" style={{ fontSize:'3rem'}}/>
-                            </div>
-                        )}
-                    </Dropzone> */}
-          {/* {ThumbnailPath &&
-                        <div>
-                            <img src={`http://localhost:5000/${ThumbnailPath}`} alt="thumbnail"/>
-                        </div>
-                    } */}
-          이미지 등록
+          <Upload action="https://www.mocky.io/v2/5cc8019d300000980a055e76" listType="picture-card" fileList={FileList} onPreview={handlePreview} onChange={handleChange}>
+            {FileList.length >= 8 ? null : uploadButton}
+          </Upload>
+          <Modal visible={PreviewVisible} title={PreviewTitle} footer={null} onCancel={handleCancel}>
+            <img alt="example" style={{ width: "100%" }} src={PreviewImage} />
+          </Modal>
         </div>
         <br />
         <br />
